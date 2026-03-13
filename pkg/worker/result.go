61d04b2c9791075c6882220daf1f273cc31b2147
Brian Douglas <bdougie@users.noreply.github.com>
2026-03-13 05:56:03 -0700

    feat: worker task and result types

---
+++ pkg/worker/result.go
@@ -0,0 +1,14 @@
+package worker
+
+import "time"
+
+type Result struct {
+	TaskID    int
+	File      string
+	Success   bool
+	Output    string
+	Error     string
+	Duration  time.Duration
+	IssuesFix int
+	IssuesNew int
+}
---
+++ pkg/worker/task.go
@@ -0,0 +1,26 @@
+package worker
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/papercomputeco/sweeper/pkg/linter"
+)
+
+type Task struct {
+	ID     int
+	File   string
+	Dir    string
+	Issues []linter.Issue
+	Prompt string
+}
+
+func BuildPrompt(task Task) string {
+	var b strings.Builder
+	b.WriteString("Fix the following lint issues in " + task.File + ":\n\n")
+	for _, iss := range task.Issues {
+		fmt.Fprintf(&b, "- Line %d: %s (%s)\n", iss.Line, iss.Message, iss.Linter)
+	}
+	b.WriteString("\nFix each issue. Do not change behavior. Only fix lint issues. Commit nothing.")
+	return b.String()
+}
